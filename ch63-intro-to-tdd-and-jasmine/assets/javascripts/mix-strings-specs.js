50f01159b8754ec5a70db59df268c0c2f9e7b4fe
Nikos Athanasakis <emidekol@gmail.com>
2017-04-19 17:49:14 +0300

    test: Added all specs (without nesting)

--- ch63-intro-to-tdd-and-jasmine/assets/javascripts/mix-strings-specs.js
+++ ch63-intro-to-tdd-and-jasmine/assets/javascripts/mix-strings-specs.js
@@ -18,8 +18,9 @@ beforeAll(function () {
 });
 
 describe('mixStrings', function () {
-  it('should be a function', function () {
+  it('should be the name of a function', function () {
     expect(mixStrings).toBeFunction();
+    // Alternative
     // expect(typeof mixStrings).toBe('function');
   });
 
@@ -35,4 +36,29 @@ describe('mixStrings', function () {
       mixStrings('str1', 2);
     }).toThrowError(ArgumentError);
   });
+
+  it('should mix its arguments and return the mixed string', function () {
+    expect(mixStrings('foo', 'bar')).toBe('fboaor');
+    expect(mixStrings('f', 'b')).toBe('fb');
+    expect(mixStrings('d!g4S', 'F4ms5')).toBe('dF!4gm4sS5');
+  });
+
+  // TODO: Add nesting
+  it('should handle cases where one or both arguments, are "", null or undefined',
+    function () {
+      // First string empty "" or null or undefined and second string non-empty.
+      // It should return the second string.
+      expect(mixStrings('', 'b')).toBe('b');
+      expect(mixStrings(null, 'f')).toBe('f');
+      expect(mixStrings(undefined, 'bar')).toBe('bar');
+      // First string is non-empty and second string is empty "" or null or
+      // undefined. It should return the first string.
+      expect(mixStrings('bar', '')).toBe('bar');
+      expect(mixStrings('f', null)).toBe('f');
+      expect(mixStrings('b', undefined)).toBe('b');
+      // When both strings are empty "" or null or undefined it should return
+      // an empty string "".
+      expect(mixStrings('', undefined)).toBe('');
+      expect(mixStrings(null, '')).toBe('');
+    });
 });
