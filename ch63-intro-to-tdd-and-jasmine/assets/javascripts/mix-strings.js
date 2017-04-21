a42e5a4f731328d9588c97c7ed69b89c991b6d44
Nikos Athanasakis <emidekol@gmail.com>
2017-04-22 02:40:44 +0300

    refactor: Renamed isNonEmptyString()

--- ch63-intro-to-tdd-and-jasmine/assets/javascripts/mix-strings.js
+++ ch63-intro-to-tdd-and-jasmine/assets/javascripts/mix-strings.js
@@ -1,4 +1,4 @@
-function isNonEmptyString(s) {
+function isValidString(s) {
   if (typeof s !== 'string' || s === '') {
     return false;
   } else {
@@ -8,16 +8,16 @@ function isNonEmptyString(s) {
 
 function mixStrings(s1, s2) {
   var result = '';
-  var isS1NonEmptyString = isNonEmptyString(s1);
-  var isS2NonEmptyString = isNonEmptyString(s2);
-  if (isS1NonEmptyString && isS2NonEmptyString) {
+  var isS1Valid = isValidString(s1);
+  var isS2Valid = isValidString(s2);
+  if (isS1Valid && isS2Valid) {
     // Assuming they have the same length
     for (var i = 0; i < s1.length; i++) {
       result += s1[i] + s2[i];
     }
-  } else if (isS1NonEmptyString) {
+  } else if (isS1Valid) {
     result = s1;
-  } else if (isS2NonEmptyString) {
+  } else if (isS2Valid) {
     result = s2;
   } // else, in case both conditions are false, `result` stays ''
 
