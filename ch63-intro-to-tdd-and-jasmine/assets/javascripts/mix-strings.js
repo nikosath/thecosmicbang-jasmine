bd7e8aec7d2ae21f0a2a4a48c0cfd0260ec7729a
Nikos Athanasakis <emidekol@gmail.com>
2017-04-21 05:26:07 +0300

    refactor: Reduced calls to isNonEmptyString func

--- ch63-intro-to-tdd-and-jasmine/assets/javascripts/mix-strings.js
+++ ch63-intro-to-tdd-and-jasmine/assets/javascripts/mix-strings.js
@@ -8,14 +8,16 @@ function isNonEmptyString(s) {
 
 function mixStrings(s1, s2) {
   var result = '';
-  if (isNonEmptyString(s1) && isNonEmptyString(s2)) {
+  var isS1NonEmptyString = isNonEmptyString(s1);
+  var isS2NonEmptyString = isNonEmptyString(s2);
+  if (isS1NonEmptyString && isS2NonEmptyString) {
     // Assuming they have the same length
     for (var i = 0; i < s1.length; i++) {
       result += s1[i] + s2[i];
     }
-  } else if (isNonEmptyString(s1)) {
+  } else if (isS1NonEmptyString) {
     result = s1;
-  } else if (isNonEmptyString(s2)) {
+  } else if (isS2NonEmptyString) {
     result = s2;
   }
 
